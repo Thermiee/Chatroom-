335ec87e9e00848e99b44b34d4f1e84beddf3b28
thermiee <navyblack16@yahoo.com>
2021-09-21 03:04:55 +0100

    created SignUp and custom errors for error message

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,5 +1,14 @@
-module.exports.signup = (req, res) => {
-    res.send('signup')
+const User = require('../models/User');
+module.exports.signup = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const user = await User.create({ name, email, password });
+        res.status(201).json({ user });
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('Fail to create user')
+    }
+
 }
 module.exports.login = (req, res) => {
     res.send('login')
