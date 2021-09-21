95fcbf01654fb114a1f3b9d458a23618d3e7dd46
thermiee <navyblack16@yahoo.com>
2021-09-21 03:19:57 +0100

    Deal with the duplicate account error

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,12 +1,25 @@
 const User = require('../models/User');
+const alertError = (err) => {
+    let errors = { name: '', email: '', password: '' }
+    // console.log(`error message: ${err.message}`)
+    // console.log(`error code: ${err.code}`)
+    // console.log('err', err)
+    if (err.message.includes('user validation failed')) {
+
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message
+        })
+    }
+    return errors
+}
 module.exports.signup = async (req, res) => {
     const { name, email, password } = req.body;
     try {
         const user = await User.create({ name, email, password });
         res.status(201).json({ user });
     } catch (error) {
-        console.log(error)
-        res.status(400).send('Fail to create user')
+        let errors = alertError(error);
+        res.status(400).json({ errors });
     }
 
 }
