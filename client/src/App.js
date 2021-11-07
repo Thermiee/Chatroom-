31099d9a7135bc73aa4428298da2d5ff15c4c113
thermiee <navyblack16@yahoo.com>
2021-11-07 04:33:51 +0100

    created Login and Verify the user with the JWT

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserContext } from './UserContext';
 import Chat from './components/chat/Chat';
 import Home from './components/home/Home';
 import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
-
 function App() {
   const [user, setUser] = useState(null)
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/verifyuser', {
+          credentials: 'include',
+          headers: { 'Content-Type': 'application/json' }
+        });
+        const data = await res.json();
+        setUser(data);
+      } catch (error) {
+        console.log(error)
+      }
+
+
+    }
+    verifyUser()
+  }, [])
   return (
     <Router>
       <div className="App">
